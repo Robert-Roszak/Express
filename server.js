5546d8ef0a96712680e8f2b8bddeffd9515208dc
Robert-Roszak <79662439+Robert-Roszak@users.noreply.github.com>
2021-10-30 18:01:04 +0200

    Add handlebars and layouts

--- server.js
+++ server.js
@@ -1,35 +1,48 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
+app.set('view engine', '.hbs');
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
 
 app.use('/user', (req, res, next) => {
-  res.show('forbidden.html');
+  res.render('forbidden');
 });
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/home', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
+});
+
+app.get('/info', (req, res) => {
+  res.render('info', {layout: 'dark'});
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
 });
 
 app.use((req, res) => {
-  res.status(404).show('404.html');
+  res.status(404).render('404');
 })
 
 app.listen(8000, () => {
