41ef5fd3dc13d07d62074cdc3184f5407c538a40
Robert-Roszak <79662439+Robert-Roszak@users.noreply.github.com>
2021-10-30 20:05:05 +0200

    Add contact form and display of sent pic

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const multer  = require('multer')
+const upload = multer({ dest: 'public/uploads' })
 const path = require('path');
 const hbs = require('express-handlebars');
 
 const app = express();
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
+app.use(express.urlencoded({ extended: false }));
 
 
 app.use('/user', (req, res, next) => {
@@ -39,6 +42,18 @@ app.get('/info', (req, res) => {
 
 app.get('/hello/:name', (req, res) => {
   res.render('hello', { name: req.params.name });
+});
+
+app.post('/contact/send-message', upload.single('projectDesign'), (req, res) => {
+  const { author, sender, title, message } = req.body;
+
+  if(author && sender && title && message && req.file) {
+    res.render('contact', { isSent: true, fileName: req.file.originalname, uploadedPic: req.file.filename });
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
+
 });
 
 app.use((req, res) => {
